b1ca0f919b7240e2aa04c6c00528d5e4caced3d7
Bruno Bronosky <Bruno@Bronosky.com>
2018-11-12 14:02:03 -0500

    Internalize the cmdline.txt, enable rebooting
    There is certainly room for improvement in the reboot wrapper pattern I
    just wanted to get something out the door. Pull Requests welcomed!

--- src/pi-init2/init.go
+++ src/pi-init2/init.go
@@ -20,21 +20,42 @@
 package main
 
 import (
-	"golang.org/x/sys/unix"
 	"fmt"
+	"golang.org/x/sys/unix"
 	"io"
-	"os"
 	"io/ioutil"
+	"os"
+	"os/exec"
 	"strings"
 	"syscall" // for Exec only
 	"time"
 )
 
 var (
-	exists = []syscall.Errno{syscall.EEXIST}
+	exists               = []syscall.Errno{syscall.EEXIST}
 	service_install_path = "/lib/systemd/system/"
 	service_enable_path  = "/etc/systemd/system/multi-user.target.wants/"
+	// Consider getting these from /dev/disk/by-partuuid/ or use root=/dev/mmcblk0p2
+	partuuid_jessie  = "a8790229-02"
+	partuuid_stretch = "4d3ee428-02"
+	cmdline_template = "dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=PARTUUID=%s rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh"
 )
+
+func debugShell() {
+	// This does not currently work. Gives the following error
+	// bash: cannot set terminal process group (-1) inappropriate ioctl for device
+	fmt.Printf("Starting a bash shell...")
+	cmd := exec.Command("/bin/bash", "-i")
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	fmt.Printf("Command finished with error: %v", err)
+
+	fmt.Printf("Rebooting in 10 seconds...")
+	time.Sleep(10 * time.Second)
+	reboot()
+}
 
 func checkFatalAllowed(desc string, err error, allowedErrnos []syscall.Errno) {
 	if err != nil {
@@ -77,15 +98,22 @@ func cp(dst, src string) error {
 	return d.Close()
 }
 
-func create_file(filename string, permissions os.FileMode, contents string) {
-	ioutil.WriteFile(filename, []byte(strings.TrimLeft(contents, "\r\n\t ")), permissions)
+func create_file(filename string, permissions os.FileMode, contents string) error {
+	err := ioutil.WriteFile(filename, []byte(strings.TrimLeft(contents, "\r\n\t ")), permissions)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func create_service(name, contents string) error {
 	src := service_install_path + name + ".service"
-	dst := service_enable_path  + name + ".service"
-	create_file(src, 0644, contents)
-	err := os.Symlink(src, dst)
+	dst := service_enable_path + name + ".service"
+	err := create_file(src, 0644, contents)
+	if err != nil {
+		return err
+	}
+	err = os.Symlink(src, dst)
 	if err != nil {
 		return err
 	}
@@ -98,10 +126,10 @@ func remount_rw() {
 		unix.Chdir("/"))
 	checkFatal(
 		"remount rw",
-		unix.Mount("/", "/", "vfat", syscall.MS_REMOUNT, ""), )
+		unix.Mount("/", "/", "vfat", syscall.MS_REMOUNT, ""))
 }
 
-func mount_tmp(){
+func mount_tmp() {
 	checkFatalAllowed(
 		"making tmp",
 		unix.Mkdir("tmp", 0770),
@@ -111,24 +139,24 @@ func mount_tmp(){
 		unix.Mount("", "tmp", "tmpfs", 0, ""))
 }
 
-func mount_root(){
+func mount_root() {
 	checkFatalAllowed(
 		"making new_root",
 		unix.Mkdir("new_root", 0770),
 		exists)
 	checkFatal(
 		"create device node",
-		unix.Mknod("tmp/mmcblk0p2", 0660 | syscall.S_IFBLK, 179<<8 | 2))
+		unix.Mknod("tmp/mmcblk0p2", 0660|syscall.S_IFBLK, 179<<8|2))
 	checkFatal(
 		"mounting real root",
 		unix.Mount("tmp/mmcblk0p2", "new_root", "ext4", 0, ""))
 }
 
-func adjust_mounts(){
+func adjust_mounts() {
 	checkFatal(
 		"pivoting",
 		unix.PivotRoot("new_root", "new_root/boot")) // new_root becomes root FS & current root FS moves to new_root/boot
-		// See: https://linux.die.net/man/8/pivot_root
+	// See: https://linux.die.net/man/8/pivot_root
 	checkFatal(
 		"unmounting /boot/tmp",
 		unix.Unmount("/boot/tmp", 0))
@@ -143,21 +171,37 @@ func adjust_mounts(){
 		unix.Chdir("/boot"))
 }
 
-func replace_cmdline(){
+func replace_cmdline() {
 	checkFatal(
 		"renaming cmdline.txt to cmdline.txt.pi-init2",
 		unix.Rename("/boot/cmdline.txt", "/boot/cmdline.txt.pi-init2"))
+
+	b, err := ioutil.ReadFile("/usr/lib/os-release")
 	checkFatal(
-		"renaming cmdline.txt.orig to cmdline.txt",
-		unix.Rename("/boot/cmdline.txt.orig", "/boot/cmdline.txt"))
+		"reading /usr/lib/os-release",
+		err)
+
+	str := string(b) // convert content bytes to a 'string'
+	if strings.Contains(str, "(jessie)") {
+		fmt.Println("it's jessie")
+		checkFatal(
+			"writing new cmdline.txt",
+			create_file("cmdline.txt", 0644, fmt.Sprintf(cmdline_template, partuuid_jessie)))
+	}
+	if strings.Contains(str, "(stretch)") {
+		fmt.Println("it's stretch")
+		checkFatal(
+			"writing new cmdline.txt",
+			create_file("cmdline.txt", 0644, fmt.Sprintf(cmdline_template, partuuid_stretch)))
+	}
 }
 
-func reboot(){
+func reboot() {
 	unix.Sync()
 	unix.Reboot(unix.LINUX_REBOOT_CMD_RESTART)
 }
 
-func customize(){
+func customize() {
 	checkFatal(
 		"changing into boot directory",
 		unix.Chdir("/boot"))
@@ -174,41 +218,55 @@ func customize(){
 		unix.Mkdir("run-once.d/completed", 0770),
 		exists)
 	create_file("/usr/local/sbin/pi-init2-run-parts.sh", 0744, `
-#!/bin/bash
+#!/bin/bash -eux
 
 # Prevent *.sh from returning itself if there are no matches
 shopt -s nullglob
 
+# DISCLAIMER: I don't love this reboot solution. It was challenging to allow the user to use nested scripts and intercept the reboot from anywhere.
+# A wrapper allow 'reboot' to be a valid completion of a task
+# To use the bypass the wrapper and use the real executable, use /sbin/reboot (but even that is a symlink to /bin/systemctl)
+tmp=$(mktemp --tmpdir -d pi-init2-XXX)
+chmod a+rx $tmp
+cat > $tmp/reboot <<'EOF'
+#!/bin/bash -eux
+  mv $(tail -n1 $(dirname $BASH_SOURCE)/runlist) /boot/run-once.d/completed/
+  /sbin/halt --reboot
+EOF
+chmod a+x $tmp/reboot
+export PATH=$tmp:$PATH
+
+# write a log
+date="$(date +%F@%H.%M.%S)"
+logfile="/boot/run-once.d/completed/logfile-${date}.txt"
+touch $logfile
+exec > >(tee -i $logfile)
+exec 2>&1
+
 # Allow lazily named scripts to work
-for script in /boot/run-once*; do
-	if [[ -f $script ]]; then
-		$script
-		status=$?
-		if $(exit $status); then
-			mv $script /boot/run-once.d/completed/
-		fi
-	fi
+for script in /boot/run-once* $(run-parts --test /boot/run-once.d); do
+    if [[ -f $script ]]; then
+        echo "run-parts: executing $script"
+        incdir=/boot/run-once.d/incomplete
+        mkdir -p $incdir
+        inc=$incdir/$(basename $script)
+        mv $script $inc
+        echo $inc>>$tmp/runlist
+        $inc
+        status=$?
+        if $(exit $status); then
+            mv $inc /boot/run-once.d/completed/
+            rm -d $incdir || true
+	else
+	    echo "WARNING: $script exited with status $status"
+        fi
+    fi
 done
 
-# Run every run-once script
-run-parts --exit-on-error /boot/run-once.d 2>/tmp/completed
-sed -i '/^run-parts: executing/!d;s/^run-parts: executing *//' /tmp/completed
-
-# Pop last script off the list if run-parts exited on an error
-status=$?
-if ! $(exit $status); then
-	sed -i '$d' /tmp/completed
-fi
-
-# Move completed scripts
-while read script; do
-	mv $script /boot/run-once.d/completed/
-done < /tmp/completed
-
 # Run every on-boot script
-run-parts /boot/on-boot.d
+run-parts -v /boot/on-boot.d
 `)
-	create_service("pi-init2",`
+	create_service("pi-init2", `
 [Unit]
 Description=Run user provided scripts on boot
 ConditionPathExists=/usr/local/sbin/pi-init2-run-parts.sh
@@ -229,12 +287,8 @@ func main() {
 	mount_tmp()
 	mount_root()
 	adjust_mounts()
+	//debugShell()
 	customize()
 	replace_cmdline()
-/*
-	checkFatal(
-		"exec real init",
-		syscall.Exec("/sbin/init", os.Args, nil))
-*/
 	reboot()
 }
