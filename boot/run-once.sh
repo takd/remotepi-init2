cf7e22c09969d3217de3934d6ee69125159e4c05
takd <takacs.donat01@gmail.com>
2019-08-27 15:13:30 +0200

    Minor changes

--- boot/run-once.sh
+++ boot/run-once.sh
@@ -9,19 +9,24 @@
 #   them ran.
 #   See: http://manpages.ubuntu.com/manpages/bionic/man8/run-parts.8.html
 
+echo "Starting run-once.sh..."
 
+echo "Updating hostname..."
 #### Update hostname
 ##  See https://raspberrypi.stackexchange.com/a/66939/8375 for a list of all the raspi-config magic you may want ot automate.
 raspi-config nonint do_hostname "$(cat /boot/hostname)"
 
+echo "Updating locale, keyboard, WiFi country..."
 ### Update locale, keyboard, WiFi country
 raspi-config nonint do_change_locale "hu_HU.UTF-8"
 raspi-config nonint do_configure_keyboard "hu"
 raspi-config nonint do_wifi_country "HU"
 
+echo "Enabling UART..."
 ### Enable UART for status LED
 raspi-config nonint do_onewire 1
 
+echo "Setting up WiFi..."
 #### Wifi Setup (WPA Supplicant)
 ##  Replaces the magic of https://github.com/RPi-Distro/raspberrypi-net-mods/blob/master/debian/raspberrypi-net-mods.service
 ##  See: https://www.raspberrypi.org/documentation/configuration/wireless/wireless-cli.md
@@ -31,6 +36,7 @@ wpa_cli -i wlan0 reconfigure
 
 rm /boot/network.conf
 
+echo "Setting up SSH..."
 #### SSH Daemon Setup
 ##  Replaces the magic of https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/master/debian/raspberrypi-sys-mods.sshswitch.service
 ##  See also: https://github.com/RPi-Distro/raspberrypi-sys-mods/blob/master/debian/raspberrypi-sys-mods.regenerate_ssh_host_keys.service
@@ -39,16 +45,19 @@ dd if=/dev/hwrng of=/dev/urandom count=1 bs=4096
 rm -f -v /etc/ssh/ssh_host_*_key*
 /usr/bin/ssh-keygen -A -v
 
+echo "Setting up services..."
 #### Setup own services
 mv /boot/services /home/pi/services
 chmod +x /home/pi/services/DS1302/setDateToRTC.py
 chmod +x /home/pi/services/DS1302/getDateFromRTC.py
 
+echo "Setting up RTC..."
 ### Setup RTC time load at startup
 echo "$(sed '$ i\/home/pi/services/DS1302/getDateFromRTC.py' /etc/rc.local)" > /etc/rc.local
 ### Run it too
 /home/pi/services/DS1302/getDateFromRTC.py
 
+echo "Setting up shutdown, wifi indicator"
 ### Copy shutdown button and wifi indicator services
 ## http://www.diegoacuna.me/how-to-run-a-script-as-a-service-in-raspberry-pi-raspbian-jessie/
 cp /home/pi/shutdown-button.service /lib/systemd/system/shutdown-button.service
@@ -56,6 +65,7 @@ chmod 644 /lib/systemd/system/shutdown-button.service
 cp /home/pi/wifi-checker.service /lib/systemd/system/wifi-checker.service
 chmod 644 /lib/systemd/system/wifi-checker.service
 
+echo "Enabling new services"
 ### Enable the new services
 sudo systemctl daemon-reload
 sudo systemctl enable shutdown-button.service
@@ -63,6 +73,9 @@ sudo systemctl start shutdown-button.service
 sudo systemctl enable wifi-checker.service
 sudo systemctl start wifi-checker.service
 
+echo "Additional scripts"
 #### Get additional scripts for subsequent usage, get will be run manually
 mv /boot/get-remotepi-scripts /home/pi/get-remotepi-scripts
 chmod +x get-remotepi-scripts
+
+echo "All done, moving on!"
